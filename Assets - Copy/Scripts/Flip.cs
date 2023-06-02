6a59efc57744de8c672a7987b05f429fbf4cc170
jayesh badgujar <jayeshbadgujar7052@gmail.com>
2023-06-02 20:23:58 +0530

    13

---
+++ Assets - Copy/Scripts/Flip.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{   
+    public Color newColor;
+    private Color originalColor;
+    private bool isdown=false;
+    private bool blek=false;
+
+    // [SerializeField]
+    public int x,y;
+
+    private void Start()
+    {   
+        originalColor=GetComponent<SpriteRenderer>().color;
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+
+    // private void OnMouseEnter()
+    // {   
+    //     if(isdown){
+    //         GetComponent<SpriteRenderer>().color = newColor;
+    //     }
+    //     if(blek){
+    //         GetComponent<SpriteRenderer>().color = originalColor;
+    //     }
+    // }
+    // private void OnMouseDown(){
+    //         // GetComponent<SpriteRenderer>().color=Color.green;
+    //         // floodFill();
+    // }
+    
+    void Update(){
+        // Draw();
+        // if(Input.GetMouseButtonDown(0)){
+        //     startflood=true;
+        // }
+        // if(Input.GetMouseButtonUp(0)){
+        //     startflood=false;
+        // }
+    }
+
+    void Draw(){
+        if (Input.GetMouseButtonDown(0)) 
+        {   
+            isdown=true;
+        }
+        if (Input.GetMouseButtonUp(0)) 
+        {   
+            isdown=false;
+        }
+        if(Input.GetMouseButtonDown(1)){
+            blek=true;
+        }
+        if(Input.GetMouseButtonUp(1)){
+            blek=false;
+        }
+    }
+
+    void floodFill(){
+        //first find pixel which is green
+        for(int i=0;i<19;i++){
+            for(int j=0;j<26;j++){
+                if(MET.myArray[i,j].GetComponent<SpriteRenderer>().color==Color.green){
+                    MET.myArray[i,j].GetComponent<SpriteRenderer>().color=MET.zero;
+                    floody(i,j);
+                    break;
+                }
+            }
+        }
+    }
+    void floody(int row, int column)
+    {
+        // Check if the current cell is within the bounds of the array
+        if (row < 0 || row >= 19 || column < 0 || column >= 26) {
+            return;
+        }
+
+        // Check if the current cell is already filled or not
+        if (MET.myArray[row, column].GetComponent<SpriteRenderer>().color != MET.zero) {
+            return;
+        }
+
+        // Fill the current cell with the new color
+        MET.myArray[row, column].GetComponent<SpriteRenderer>().color = Color.green;
+
+        StartCoroutine(DelayedFunction(row-1,column));
+        StartCoroutine(DelayedFunction(row+1,column));
+        StartCoroutine(DelayedFunction(row,column-1));
+        StartCoroutine(DelayedFunction(row,column+1));
+        // Recursively fill the neighboring cells
+    }
+
+    IEnumerator DelayedFunction(int row,int column)
+    {
+        yield return new WaitForSeconds(0.2f);
+        Debug.Log("Delayed function executed");
+        floody(row, column);
+    }
+
+}
---
+++ Assets - Copy/Scripts/SETDSTSRC.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SETDSTSRC : MonoBehaviour
+{
+    private GameObject prevSRC;
+    private GameObject prevDST;
+    private bool srcass=false;
+    private bool dstass=false;
+
+    public static Color cc = Color.blue;
+   
+    public static Color rr = Color.green;
+
+    public Color psrc=MET.zero;
+
+    public Color pdst=MET.zero;
+
+    public static SETDSTSRC sds;
+
+    private GameObject spawn;
+
+    public static int sx=0,sy=0,dx=18,dy=25;
+
+    [SerializeField]
+    public testconfirmation tc;
+
+    public TextMeshProUGUI sceneName;
+    private void Start()
+    {  
+       sceneName=FindObjectOfType<TextMeshProUGUI>();
+       sceneName.text="SRC/DST";
+       sx=0;sy=0;dx=MET.ROW;dy=MET.COL; 
+       spawn=GameObject.FindWithTag("blk"); 
+    //    if(sds!=null){
+    //         Destroy(this.gameObject);
+    //         return;
+    //    }
+    //    sds=this;
+    //    GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(clickPosition, Vector3.forward);
+            if (hit.collider != null && hit.transform.gameObject.tag=="blk")
+            {   
+                if(prevSRC){
+                    Destroy(prevSRC);
+                }
+                    GameObject hitObject = hit.collider.gameObject;
+
+                    psrc=hitObject.GetComponent<SpriteRenderer>().color;
+
+                    Vector3 spawnPosition = hitObject.transform.position;
+                    prevSRC=Instantiate(spawn, spawnPosition, Quaternion.identity); 
+                    prevSRC.GetComponent<SpriteRenderer>().color=cc;
+                    prevSRC.GetComponent<Renderer>().sortingOrder=10;
+                    sx=hitObject.GetComponent<Flip>().x;
+                    sy=hitObject.GetComponent<Flip>().y;
+            }
+        }
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(clickPosition, Vector3.forward);
+            if (hit.collider != null && hit.transform.gameObject.tag=="blk")
+            {   
+                if(prevDST){
+                    Destroy(prevDST);
+                }
+                    GameObject hitObject = hit.collider.gameObject;
+
+                    pdst=hitObject.GetComponent<SpriteRenderer>().color;
+
+                    if(psrc!=pdst || psrc==MET.zero){
+                        tc.openconfirmationwindow("invalid h brohh!");
+                    }else
+                    {
+                    Vector3 spawnPosition = hitObject.transform.position;
+                    prevDST=Instantiate(spawn, spawnPosition, Quaternion.identity); 
+                    prevDST.GetComponent<SpriteRenderer>().color=rr;
+                    prevDST.GetComponent<Renderer>().sortingOrder=10;
+                    dx=hitObject.GetComponent<Flip>().x;
+                    dy=hitObject.GetComponent<Flip>().y;
+                    }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.R)){
+            if(prevDST){
+                Destroy(prevDST);
+            }
+            if(prevSRC){
+                Destroy(prevSRC);
+            }
+        }
+    }
+}
