1c177c2d76b4b8221ce8cceac73b3be256e13a6c
jayesh badgujar <jayeshbadgujar7052@gmail.com>
2023-06-10 04:49:10 +0530

    15

---
+++ Assets/Scripts/ButtonScript/bfs_button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class bfs_button : MonoBehaviour
+{
+    public static bfs_button bb;
+    void Start()
+    {
+        if(bb!=null){
+            Destroy(this.gameObject);
+            return;
+        }
+        bb=this;
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+    void OnMouseDown(){
+        Debug.Log("entered in bfs");
+        SceneManager.LoadScene("bfs");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
