75266ed319cc263cba265dd4971cca76c9d23396
jayesh badgujar <jayeshbadgujar7052@gmail.com>
2023-06-10 09:41:20 +0530

    16

--- Assets/Scripts/bfs.cs
+++ Assets/Scripts/bfs.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public struct Pair<T1, T2>
+{
+    public T1 First { get; set; }
+    public T2 Second { get; set; }
+
+    public Pair(T1 first, T2 second)
+    {
+        this.First = first;
+        this.Second = second;
+    }
+}
 
 public class bfs : MonoBehaviour
 {
-    // Start is called before the first frame update
+    List<Pair<int,int>> corr = new List<Pair<int, int>>();
+    List<Pair<int,int>> voi = new List<Pair<int, int>>();       
     void Start()
     {
-        
+        for(int i=0;i<MET.ROW;i++){
+            for(int j=0;j<MET.COL;j++){
+                 corr.Add(new Pair<int, int>(i, j));
+                Debug.Log(a[]);
+            }
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
