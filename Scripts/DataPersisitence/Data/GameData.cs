06425590aa36a3b1398cb9d70693ef636dbe7613
jayesh badgujar <jayeshbadgujar7052@gmail.com>
2023-12-24 01:20:17 +0530

    rotation

---
+++ Scripts/DataPersisitence/Data/GameData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class GameData 
+{   
+
+        [SerializeField]
+  
+    public List<KeyValuePair<int, int>> met1 = new List<KeyValuePair<int, int>>();
+    public List<KeyValuePair<int, int>> met2 = new List<KeyValuePair<int, int>>();
+    public List<KeyValuePair<int, int>> met3 = new List<KeyValuePair<int, int>>();
+    public List<KeyValuePair<int, int>> met4 = new List<KeyValuePair<int, int>>();
+    public List<KeyValuePair<int, int>> met5 = new List<KeyValuePair<int, int>>();
+    public int deathCount;
+    // public SerializableDictionary<string,int> metsaw;
+
+    public GameData(){
+        this.deathCount=0;
+ 
+        
+    }
+}
---
+++ Scripts/DataPersisitence/SerializableTypes/SerializableDictinary.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+{
+
+    [SerializeField] public List<TKey> keys = new List<TKey>();
+    [SerializeField] public List<TValue> values = new List<TValue>();
+
+    // save the dictionary to lists
+    public void OnBeforeSerialize()
+    {
+        keys.Clear();
+        values.Clear();
+        foreach (KeyValuePair<TKey, TValue> pair in this) 
+        {
+            keys.Add(pair.Key);
+            values.Add(pair.Value);
+        }
+    }
+
+    // load the dictionary from lists
+    public void OnAfterDeserialize()
+    {
+        this.Clear();
+
+        if (keys.Count != values.Count) 
+        {
+            Debug.LogError("Tried to deserialize a SerializableDictionary, but the amount of keys ("
+                + keys.Count + ") does not match the number of values (" + values.Count 
+                + ") which indicates that something went wrong");
+        }
+
+        for (int i = 0; i < keys.Count; i++) 
+        {
+            this.Add(keys[i], values[i]);
+        }
+    }
+
+}
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// [System.Serializable]
+// public class SerializableDictinary <TKey,TValue>:Dictionary<TKey,TValue>,ISerializationCallbackReceiver
+// {
+
+//         [SerializeField] private List<TKey> keys=new List<TKey>();
+//         [SerializeField] private List<TValue> value=new List<TValue>();
+//         public void OnBeforeSerialize(){
+//                 keys.Clear();
+//                 values.Clear();
+//                 foreach (KeyValuePair<TKey, TValue> pair in this) 
+//                 {
+//                     keys.Add(pair.Key);
+//                     values.Add(pair.Value);
+//                 }
+//         }
+//         public void OnAfterDeserialize(){
+//             this.Clear();
+//                 if(keys.Count!=values.Count){
+//                     Debug.Log("keys and values not equal");
+//                 }
+//                 for(int i=0;i<keys.Count;i++){
+//                     this.Add(keys[i],value[i]);
+//                 }
+//         }
+// }
