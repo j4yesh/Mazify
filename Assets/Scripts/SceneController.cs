c248b39cf195e924158fae951bfc5feace7d1768
jayesh badgujar <jayeshbadgujar7052@gmail.com>
2023-12-24 12:17:49 +0530

    dheeme

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneController : MonoBehaviour,IDataPersistenceManger
+public class SceneController : MonoBehaviour, IDataPersistenceManger
 {
     private int[,] save1 = new int[100, 100];
-   
+
     void Start()
-    {   
-       
+    {
+
         // if(dinst!=null){
         //     Destroy(this.gameObject);
         //     return;
@@ -18,250 +18,381 @@ public class SceneController : MonoBehaviour,IDataPersistenceManger
         // GameObject.DontDestroyOnLoad(gameObject);
     }
 
-    public void changeDraw(){
+    public void changeDraw()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="General")
+        if (SceneManager.GetActiveScene().name != "General")
             SceneManager.LoadScene("General");
     }
 
-    public void changeSnake(){
+    public void changeSnake()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="snakeGame")
-                SceneManager.LoadScene("snakeGame");
+        if (SceneManager.GetActiveScene().name != "snakeGame")
+            SceneManager.LoadScene("snakeGame");
     }
 
-    public void setDSTandSRC(){
+    public void setDSTandSRC()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="setDSTandSRC")
-                SceneManager.LoadScene("setDSTandSRC");
-    } 
+        if (SceneManager.GetActiveScene().name != "setDSTandSRC")
+            SceneManager.LoadScene("setDSTandSRC");
+    }
 
-    public void Backtracking(){
+    public void Backtracking()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="Backtracking")
-                SceneManager.LoadScene("Backtracking");
-    } 
+        if (SceneManager.GetActiveScene().name != "Backtracking")
+            SceneManager.LoadScene("Backtracking");
+    }
 
-    public void bfs(){
+    public void bfs()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="bfs")
-                SceneManager.LoadScene("bfs");
-    } 
+        if (SceneManager.GetActiveScene().name != "bfs")
+            SceneManager.LoadScene("bfs");
+    }
 
 
-    public void dfs(){
+    public void dfs()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="dfs")
-                SceneManager.LoadScene("dfs");
-    } 
+        if (SceneManager.GetActiveScene().name != "dfs")
+            SceneManager.LoadScene("dfs");
+    }
 
 
-    public void dijkshtra(){
+    public void dijkshtra()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="dijkshtra")
-                SceneManager.LoadScene("dijkshtra");
-    } 
+        if (SceneManager.GetActiveScene().name != "dijkshtra")
+            SceneManager.LoadScene("dijkshtra");
+    }
 
 
-    public void dpflood(){
+    public void dpflood()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="dpflood")
-                SceneManager.LoadScene("dpflood");
-    } 
+        if (SceneManager.GetActiveScene().name != "dpflood")
+            SceneManager.LoadScene("dpflood");
+    }
 
-    public void Flood(){
+    public void Flood()
+    {
         Debug.Log("entered in general");
-        if(SceneManager.GetActiveScene().name!="Flood")
-                SceneManager.LoadScene("Flood");
-    } 
+        if (SceneManager.GetActiveScene().name != "Flood")
+            SceneManager.LoadScene("Flood");
+    }
 
-    public void removeSRCDST(){
-         GameObject[] objectsToDelete = GameObject.FindGameObjectsWithTag("setsrc");
+    public void SaveMenu()
+    {
+        Debug.Log("entered in general");
+        if (SceneManager.GetActiveScene().name != "SaveMenu")
+            SceneManager.LoadScene("SaveMenu");
+    }
+
+    public void removeSRCDST()
+    {
+        GameObject[] objectsToDelete = GameObject.FindGameObjectsWithTag("setsrc");
 
         foreach (GameObject obj in objectsToDelete)
-        {   
+        {
             Debug.Log("deleted");
             DestroyImmediate(obj);
         }
-            Debug.Log("removeSRCDST DESTROYed");
+        Debug.Log("removeSRCDST DESTROYed");
 
     }
 
-    public void removePath(){
-        for(int i=0;i<MET.ROW;i++){
-            for(int j=0;j<MET.COL;j++){
-                if( MET.myArray[i,j].GetComponent<SpriteRenderer>().color!=MET.one
-                    && MET.myArray[i,j].GetComponent<SpriteRenderer>().color!=MET.zero
-                ){
-            MET.myArray[i,j].GetComponent<SpriteRenderer>().color=MET.one;
+    public void removePath()
+    {
+        for (int i = 0; i < MET.ROW; i++)
+        {
+            for (int j = 0; j < MET.COL; j++)
+            {
+                if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.one
+                    && MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero
+                )
+                {
+                    MET.myArray[i, j].GetComponent<SpriteRenderer>().color = MET.one;
                     Debug.Log("destroyed for path");
                 }
             }
         }
-                    Debug.Log("REMOVE path func. invoked");
+        Debug.Log("REMOVE path func. invoked");
     }
 
-    public void resetMaze(){
-        for(int i=0;i<MET.ROW;i++){
-            for(int j=0;j<MET.COL;j++){
-            MET.myArray[i,j].GetComponent<SpriteRenderer>().color=MET.zero;
+    public void resetMaze()
+    {
+        for (int i = 0; i < MET.ROW; i++)
+        {
+            for (int j = 0; j < MET.COL; j++)
+            {
+                MET.myArray[i, j].GetComponent<SpriteRenderer>().color = MET.zero;
             }
         }
-                 
+
     }
 
-    public void saveThemaze(){
-        for(int i=0;i<MET.ROW;i++){
-            for(int j=0;j<MET.COL;j++){
-                if(MET.myArray[i,j].GetComponent<SpriteRenderer>().color!=MET.zero){
-                    save1[i,j]=1;
-                }else{
-                    save1[i,j]=0;
+    public void saveThemaze()
+    {
+        for (int i = 0; i < MET.ROW; i++)
+        {
+            for (int j = 0; j < MET.COL; j++)
+            {
+                if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero)
+                {
+                    save1[i, j] = 1;
+                }
+                else
+                {
+                    save1[i, j] = 0;
                 }
             }
         }
         Debug.Log("temporary saved successfully");
-   }
-
-    public void displaySaved(){
-        string ss="{";
-         for(int i=0;i<MET.ROW;i++)
-        {   string num="{";
-            for(int j=0;j<MET.COL;j++)
-            {
-                if(save1[i,j]==1)
-                {
-                    MET.myArray[i,j].GetComponent<SpriteRenderer>().color=MET.one;
-                    num+="1";
-                }
-                else
-                {   
-                    MET.myArray[i,j].GetComponent<SpriteRenderer>().color=MET.zero;
-                    num+="0";
-                }
-                if(j!=25){
-                    num+=',';
-                }
-            }
-            num+="}";
-            if(i!=18){
-                num+=',';
-            }
-            ss+=num;
-        }
-        // Debug.Log(ss);
-    Debug.Log("overwrited successfully");
     }
 
-    public void OnNewGameClicked(){
+    public void displaySaved()
+    {
+        string ss = "{";
+        for (int i = 0; i < MET.ROW; i++)
+        {
+            string num = "{";
+            for (int j = 0; j < MET.COL; j++)
+            {
+                if (save1[i, j] == 1)
+                {
+                    MET.myArray[i, j].GetComponent<SpriteRenderer>().color = MET.one;
+                    num += "1";
+                }
+                else
+                {
+                    MET.myArray[i, j].GetComponent<SpriteRenderer>().color = MET.zero;
+                    num += "0";
+                }
+                if (j != 25)
+                {
+                    num += ',';
+                }
+            }
+            num += "}";
+            if (i != 18)
+            {
+                num += ',';
+            }
+            ss += num;
+        }
+        // Debug.Log(ss);
+        Debug.Log("overwrited successfully");
+    }
+
+    public void OnNewGameClicked()
+    {
         DataPersistenceManager.instance.NewGame();
     }
 
-    public void OnLoadGameClicked(){
+    public void OnLoadGameClicked()
+    {
         DataPersistenceManager.instance.LoadGame();
     }
 
-    public void OnSaveGameClicked(){
+    public void OnSaveGameClicked()
+    {
         DataPersistenceManager.instance.SaveGame();
     }
 
-private int choice=-1;
- public void LoadData(GameData data)
-{
-    Debug.Log(data.deathCount);
-    foreach (KeyValuePair<int, int> it in data.met1){
-        // MET.myArray[3,3].GetComponent<SpriteRenderer>().color=MET.one;
-        Debug.Log(it.Key+" | "+it.Value);
-        if(MET.myArray[it.Key,it.Value])
-            MET.myArray[it.Key,it.Value].GetComponent<SpriteRenderer>().color=MET.one;
-    }
-    
-
-    
-
-
-}
-
-public void SaveData(ref GameData data)
-{
-    data.deathCount = 121;
-    data.met1.Clear();
-    for (int i = 0; i < MET.ROW; i++){
-        for (int j = 0; j < MET.COL; j++){
-            if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero){
-                data.met1.Add(new KeyValuePair<int, int>(i, j));
+    private int choice = -1;
+    public void LoadData(GameData data)
+    {
+        Debug.Log(data.deathCount);
+        if (choice == -1)
+        {
+            foreach (KeyValuePair<int, int> it in data.met1)
+            {
+                // MET.myArray[3,3].GetComponent<SpriteRenderer>().color=MET.one;
+                Debug.Log(it.Key + " | " + it.Value);
+                if (MET.myArray[it.Key, it.Value])
+                    MET.myArray[it.Key, it.Value].GetComponent<SpriteRenderer>().color = MET.one;
             }
         }
+        else
+        {
+
+
+            switch (choice)
+            {
+                case 2:
+                    Debug.Log("Option 2 selected");
+                    foreach (KeyValuePair<int, int> it in data.met2)
+                    {
+                        // MET.myArray[3,3].GetComponent<SpriteRenderer>().color=MET.one;
+                        Debug.Log(it.Key + " | " + it.Value);
+                        if (MET.myArray[it.Key, it.Value])
+                            MET.myArray[it.Key, it.Value].GetComponent<SpriteRenderer>().color = MET.one;
+                    }
+                    break;
+
+                case 3:
+                    Debug.Log("Option 3 selected");
+                    foreach (KeyValuePair<int, int> it in data.met3)
+                    {
+                        // MET.myArray[3,3].GetComponent<SpriteRenderer>().color=MET.one;
+                        Debug.Log(it.Key + " | " + it.Value);
+                        if (MET.myArray[it.Key, it.Value])
+                            MET.myArray[it.Key, it.Value].GetComponent<SpriteRenderer>().color = MET.one;
+                    }
+                    break;
+
+                case 4:
+                    Debug.Log("Option 4 selected");
+                    foreach (KeyValuePair<int, int> it in data.met4)
+                    {
+                        // MET.myArray[3,3].GetComponent<SpriteRenderer>().color=MET.one;
+                        Debug.Log(it.Key + " | " + it.Value);
+                        if (MET.myArray[it.Key, it.Value])
+                            MET.myArray[it.Key, it.Value].GetComponent<SpriteRenderer>().color = MET.one;
+                    }
+                    break;
+
+                case 5:
+                    Debug.Log("Option 5 selected");
+                    foreach (KeyValuePair<int, int> it in data.met5)
+                    {
+                        // MET.myArray[3,3].GetComponent<SpriteRenderer>().color=MET.one;
+                        Debug.Log(it.Key + " | " + it.Value);
+                        if (MET.myArray[it.Key, it.Value])
+                            MET.myArray[it.Key, it.Value].GetComponent<SpriteRenderer>().color = MET.one;
+                    }
+                    break;
+
+                default:
+                    Debug.Log("Invalid option selected");
+                    // Perform actions for default case (when none of the specified cases match)
+                    break;
+            }
+
+
+        }
     }
 
-
-
-
-    
-        if(choice!=-1) {      
-            switch (choice)
+    public void SaveData(ref GameData data)
+    {
+        data.deathCount = 121;
+        data.met1.Clear();
+        for (int i = 0; i < MET.ROW; i++)
         {
-            case 2:
-                Debug.Log("Option 2 selected");
-                data.met2.Clear();
-                for (int i = 0; i < MET.ROW; i++){
-                    for (int j = 0; j < MET.COL; j++){
-                        if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero){
-                            data.met2.Add(new KeyValuePair<int, int>(i, j));
-                        }
-                    }
+            for (int j = 0; j < MET.COL; j++)
+            {
+                if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero)
+                {
+                    data.met1.Add(new KeyValuePair<int, int>(i, j));
                 }
-                break;
-
-            case 3:
-                Debug.Log("Option 3 selected");
-                data.met3.Clear();
-                for (int i = 0; i < MET.ROW; i++){
-                    for (int j = 0; j < MET.COL; j++){
-                        if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero){
-                            data.met3.Add(new KeyValuePair<int, int>(i, j));
-                        }
-                    }
-                }
-                break;
-
-            case 4:
-                Debug.Log("Option 4 selected");
-                data.met4.Clear();
-                for (int i = 0; i < MET.ROW; i++){
-                    for (int j = 0; j < MET.COL; j++){
-                        if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero){
-                            data.met4.Add(new KeyValuePair<int, int>(i, j));
-                        }
-                    }
-                }
-                break;
-
-            case 5:
-                Debug.Log("Option 5 selected");
-                data.met5.Clear();
-                for (int i = 0; i < MET.ROW; i++){
-                    for (int j = 0; j < MET.COL; j++){
-                        if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero){
-                            data.met5.Add(new KeyValuePair<int, int>(i, j));
-                        }
-                    }
-                }
-                break;
-
-            default:
-                Debug.Log("Invalid option selected");
-                // Perform actions for default case (when none of the specified cases match)
-                break;
+            }
         }
 
 
 
 
 
-    Debug.Log("save data succ.");
-}
+        if (choice != -1)
+        {
+            switch (choice)
+            {
+                case 2:
+                    Debug.Log("Option 2 selected");
+                    data.met2.Clear();
+                    for (int i = 0; i < MET.ROW; i++)
+                    {
+                        for (int j = 0; j < MET.COL; j++)
+                        {
+                            if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero)
+                            {
+                                data.met2.Add(new KeyValuePair<int, int>(i, j));
+                            }
+                        }
+                    }
+                    break;
 
-    
+                case 3:
+                    Debug.Log("Option 3 selected");
+                    data.met3.Clear();
+                    for (int i = 0; i < MET.ROW; i++)
+                    {
+                        for (int j = 0; j < MET.COL; j++)
+                        {
+                            if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero)
+                            {
+                                data.met3.Add(new KeyValuePair<int, int>(i, j));
+                            }
+                        }
+                    }
+                    break;
+
+                case 4:
+                    Debug.Log("Option 4 selected");
+                    data.met4.Clear();
+                    for (int i = 0; i < MET.ROW; i++)
+                    {
+                        for (int j = 0; j < MET.COL; j++)
+                        {
+                            if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero)
+                            {
+                                data.met4.Add(new KeyValuePair<int, int>(i, j));
+                            }
+                        }
+                    }
+                    break;
+
+                case 5:
+                    Debug.Log("Option 5 selected");
+                    data.met5.Clear();
+                    for (int i = 0; i < MET.ROW; i++)
+                    {
+                        for (int j = 0; j < MET.COL; j++)
+                        {
+                            if (MET.myArray[i, j].GetComponent<SpriteRenderer>().color != MET.zero)
+                            {
+                                data.met5.Add(new KeyValuePair<int, int>(i, j));
+                            }
+                        }
+                    }
+                    break;
+
+                default:
+                    Debug.Log("Invalid option selected");
+                    break;
+            }
+
+
+
+
+
+            Debug.Log("save data succ.");
+        }
+    }
+
+    public void saveOne(){
+        choice=2;
+        DataPersistenceManager.instance.SaveGame();
+        choice=-1;
+    }
+    public void saveTwo(){
+        choice=3;
+        DataPersistenceManager.instance.SaveGame();
+        choice=-1;
+    }
+    public void saveThree(){
+        choice=4;
+        DataPersistenceManager.instance.SaveGame();
+        choice=-1;
+    }
+    public void saveFour(){
+        choice=5;
+        DataPersistenceManager.instance.SaveGame();
+        choice=-1;
+    }
+
+
 };
 
---
+++ Assets/Scripts/bagyMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class bagyMove : MonoBehaviour
+{
+    public float moveSpeed = 0.000007f;
+    Vector3 min = new Vector3(-1.2f, -0.7f, 0f);
+    Vector3 max = new Vector3(1f, 0.5f, 0f);
+
+    void Update()
+    {
+       
+        Vector3 mousePosition = Input.mousePosition;
+
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10f));
+
+        Vector3 constrainedPosition = new Vector3(
+            Mathf.Clamp(worldMousePosition.x, min.x, max.x),
+            Mathf.Clamp(worldMousePosition.y, min.y, max.y),
+            Mathf.Clamp(worldMousePosition.z, min.z, max.z)
+        );
+
+        transform.position = Vector3.MoveTowards(transform.position, constrainedPosition, moveSpeed * Time.deltaTime);
+    }
+}
